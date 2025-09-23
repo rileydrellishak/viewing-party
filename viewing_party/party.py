00896c243dc282dccd940f864fff7d4f0435d14e
Riley Drellishak <rileydrellishak@gmail.com>
2025-09-23 15:01:53 -0400

    Creates docstrings for wave 1 functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,55 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    """Create a dictionary with key-value pairs. A dictionary represents a single movie.
+    Keys are title, genre, and rating (str).
+    Values are respective titles (str), genres (str), and ratings (float) for a movie.
+    If any inputs are falsy, return None.
+    Args:
+        title (str)
+        genre (str)
+        rating (float)
+
+    Returns:
+        dict
+    """
     pass
 
+def add_to_watched(user_data, movie):
+    """Adds a dictionary of movie data to a list of user's watched movies.
+
+    Args:
+        user_data: A list of dictionaries where dictionaries represent movies user has watched.
+        movie: A dictionary with key-value pairs representing the title, genre, and rating of a watched movie.
+
+    Returns:
+        list: user_data with the movie dictionary appended.
+    """
+    pass
+
+def add_to_watchlist(user_data, movie):
+    """Adds a dictionary of movie data to a list of user's movies they want to watch.
+
+    Args:
+        user_data: A list of dictionaries where dictionaries represent movies user wants to watch.
+        movie: A dictionary with key-value pairs representing the title, genre, and rating of a movie the user wants to watch.
+
+    Returns:
+        list: user_data with the movie dictionary appended.
+    """
+    pass
+
+def watch_movie(user_data, title):
+    """Given a movie title, find the movie title in the "watchlist" value (list of movies as dictionaries). Moves a movie from "watchlist" to "watched."
+
+    Args:
+        user_data: A dictionary with two keys - "watchlist" and "watched." Respective values are lists of movies (dictionaries).
+        title (str): The title of the movie the user has watched
+
+    Return:
+        user_data: A dictionary where the values of "watchlist" and "watched" are updated to reflect a movie has passed from watchlist to watched. 
+    """
+    pass
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
