a8aa71630f1a6bf3a0bbbabeb6a66d5c16b9966b
Riley Drellishak <rileydrellishak@gmail.com>
2025-09-23 16:38:59 -0400

    Implements get_most_watched_genre function, passes all tests. Will refactor tomorrow.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -85,11 +85,21 @@ def get_most_watched_genre(user_data):
     genre_frequency_dict = {}
     for i in range(0, len(user_data["watched"])):
         if user_data["watched"][i]["genre"] not in genre_frequency_dict:
-            user_data["watched"][i]["genre"] = 1
+            genre_frequency_dict[user_data["watched"][i]["genre"]] = 1
         else:
-            user_data["watched"][i]["genre"] += 1
-    most_watched_genre = genre_frequency_dict
-    return genre_frequency_dict
+            genre_frequency_dict[user_data["watched"][i]["genre"]] += 1
+    
+    genres = list(genre_frequency_dict.keys())
+    counts = list(genre_frequency_dict.values())
+    most_frequent_count = counts[0]
+    index_of_most_frequent = 0
+    
+    for i in range(1, len(counts)):
+        if counts[i] > most_frequent_count:
+            most_frequent_count = counts[i]
+            index_of_most_frequent = i
+    
+    return genres[index_of_most_frequent]
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
