a5c58b9a3803ada60d042fc1945bc0f1bdec0808
Riley Drellishak <rileydrellishak@gmail.com>
2025-09-24 17:13:35 -0400

    Adds two asserts to test_Wave_01.py test_moves_movie_from_watchlist_to_empty_watched

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -158,11 +158,8 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    #raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert not updated_data["watchlist"]
 
 #@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
